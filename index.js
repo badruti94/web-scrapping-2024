78653a7f463410e639e12bb4769660d63cdca1b1
Badru <badruakfm@gmail.com>
2024-12-14 20:53:08 +0700

    first init

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const cors = require('cors');
+
+const app = express();
+const URL = 'https://batotoo.com/series/143694/why-ophelia-can-t-leave-the-duchy-official';
+
+app.use(cors())
+
+app.get('/', async (req, res) => {
+    try {
+        console.log('run');
+        
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        // Pergi ke halaman web target
+        await page.goto(URL, { waitUntil: 'networkidle2', timeout: 1 * 60 * 60 * 1000 });
+
+        // Mengambil konten HTML
+        // const content = await page.content();
+        // console.log(content);
+
+
+        let links = await page.$$eval('.main div.p-2 a.chapt', elements => elements.map(element => element.href));
+
+        links = links.reverse()
+
+        res.json(links)
+    } catch (error) {
+        console.log('error', error);
+        
+        res.json(error)
+    }
+});
+
+app.listen(3000, () => console.log('listen'));
